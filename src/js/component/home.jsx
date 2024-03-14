50aeec251e9c7a46e051cc3da8be8ffc9d6ba8af
lisandromariano <145061399+lisandromariano@users.noreply.github.com>
2024-03-14 19:48:29 +0000

    Done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,139 +1,168 @@
+import React, { useState, useEffect } from "react";
 
-import React, { useEffect, useState } from "react";
+
+
 
 const Home = () => {
-    const [inputValue, setInputValue] = useState("");
-    const [todos, setTodos] = useState([])
-    const apiUrl = "https://playground.4geeks.com/apis/fake/todos/user/lisandromariano"
-    const createUser = async () => {
-        try {
-            const response = await fetch(apiUrl, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify([])
-            });
-            if (response.ok) {
-                const data = await response.json();
-                console.log("usuario creado", data);
-            }
-            else { console.error("error al crear el usuario", response.status) }
-        }
-        catch (error) {
-            console.error(error);
-        }
-    }
 
-    const getAllTasks = async () => {
-        try {
-            const response = await fetch(apiUrl);
-            if (response.ok) {
-                const data = await response.json();
-                console.log(data);
-                setTodos(data)
-            }
-            else {
-                if (response.status === 404) {
-                    console.log("Usuario no encontrado");
-                    createUser();
-                } else { console.error("error en la solicitus", response.status) }
-            }
-        }
-        catch (error) { console.error }
-    }
+	const [add, setAdd] = useState("");
+	// const [list, setList] = useState(JSON.parse(localStorage.getItem("myList")) || []);
+	const [list, setList] = useState([]);
+	const [hoverIndex, setHoverIndex] = useState("");
+	const [remainingItems, setRemainingItems] = useState(list.length);
 
-    const addTask = async (value) => {
-        try {
-            const newTask = {
-                label: value, 
-                done: false
-            };
-            const updatedTask = [...todos, newTask];
-            console.log(updatedTask);
-            const putOptions = {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json", 
-                },
-                body: JSON.stringify(updatedTask), 
-            };
-            const putResponse = await fetch (apiUrl, putOptions);
-            if (putResponse.ok){
-                setInputValue("");
-                getAllTasks();
-            } else {
-                console.error("Error al agregar la tarea")
-            }
-        }
-        catch (error) {console.error("Error al agregar tarea", error)}
-    }
+	function keyIntro(e) {
 
-    const deleteTask = async (id) => {
-        const updatedList = todos.filter((task) => task.id !== id);
-        setTodos (updatedList);
-        if (updatedList.length === 0){
-            const defaultTask = {
-                id: 1, label: "Default Task", done: false, 
-            }
-            updatedList.push(defaultTask)
-        }
-        const options = {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(updatedList),
-        };
-        await fetch (apiUrl, options).then((response) => {
-            if (!response.ok){
-                console.error("Error al actualizar el API")
-            }
-        } )
-    }
+		// e.preventDefault();
+		// console.log(e.keyCode);
+		let aux = []
+		if (e.keyCode === 13) {
+			aux = list.concat({ label: add, done: false });
+			setList(list.concat({ label: add, done: false }));
+			setRemainingItems(aux.length);
+			setAdd("");
 
-useEffect(()=> {
-    getAllTasks()
-},[])
 
-    //Aca debajo en el Return empieza lo que es la parte del HTML, seria la estructura de lo que queres mostrar y el diseño.
-    //Pones el estado con el que queres que empiece,
-    //que lo declaramos arriba, al principio, en los espacios de memoria, Task y NewTask
-    return (
-        <div className="Container">
+		}
 
-            <h1>My Todos</h1>
-            <ul>
-                <li>
-                    <input 
-                        type="text" 
-                        onChange={(e) => setInputValue(e.target.value)} 
-                        value={inputValue} 
-                        onKeyDown={(e) => {
-                        if (e.key === "Enter") {
-                            console.log(inputValue);
-                            addTask (inputValue);
-                        }
-                    }}
-                    placeholder="Agregar Tarea"></input>
-                </li>
-                {
-                    todos.map((item) => {
-                        return(
-                            <li key={item.id}>
-                                {`${item.label}`}
-                                <i className="far fa-times-circle" onClick={() => {deleteTask(item.id)}}></i>
-                            </li>
-                        )
-                    }
-    
-                    )
-                }
-            </ul>
-        </div>
-    )
-}
+	};
 
+	function removeItem(index) {
+		const updatedList = [...list];
+		updatedList.splice(index, 1);
+		setList(updatedList);
+		setRemainingItems(updatedList.length);
+
+
+	}
+
+	const getUserList = () => {
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/velazcopolo')
+			.then(resp => {
+				console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+				console.log(resp.status); // el código de estado = 200 o código = 400 etc. 
+				if (resp.status === 404) {
+					createUser()
+
+				}
+				return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+				setList(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+			})
+			.catch(error => {
+				//manejo de errores
+				console.log(error);
+			});
+
+
+
+	}
+
+	const createUser = () => {
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/velazcopolo', {
+			method: "POST",
+			body: JSON.stringify([]),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+				console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+				if (resp.status === 201) {
+					getUserList()
+
+				}
+
+				return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+				console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+				return data;
+			})
+			.catch(error => {
+				//manejo de errores
+				console.log(error);
+			});
+
+	};
+
+
+
+	const updateList = () => {
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/velazcopolo', {
+			method: 'PUT', // or 'POST'
+			body: JSON.stringify(list), // la variable data puede ser un 'string' o un {objeto} que proviene de algún lugar más arriba en nuestra aplicación
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(res => {
+				if (!res.ok) throw Error(res.statusText);
+				return res.json();
+
+			})
+			.then(response => console.log('Success:', response))
+			.catch(error => console.error(error));
+
+
+
+
+	}
+
+
+	useEffect(() => {
+
+		getUserList();
+
+	}, []);
+
+
+	useEffect(() => {
+
+		if (list.length != 0) {
+			updateList();
+		}
+
+	}, [list]);
+
+
+
+	return (
+		<div className="text-center container mt-5 bg-success">
+
+			<div className="mb-3 ">
+				<h1>todos</h1>
+				<input type="text" className="form-control" value={add} placeholder="add a element" onChange={(e) => setAdd(e.target.value)} onKeyDown={keyIntro} />
+
+			</div>
+
+			<div>
+				<ul>
+					{list.length > 0 ? list.map((item, index) =>
+						<li
+							className="d-flex justify-content-between"
+							key={index}
+							onMouseEnter={() => setHoverIndex(index)}
+							onMouseLeave={() => setHoverIndex(null)}
+						>
+							{item.label}
+							{hoverIndex === index && (
+								<button onClick={() => removeItem(index)}>X</button>
+							)}
+						</li>
+					) : "no hay tareas"}
+				</ul>
+				<p>{remainingItems} items remaining</p>
+			</div>
+
+		</div>
+	);
+
+};
 
 export default Home;
 
